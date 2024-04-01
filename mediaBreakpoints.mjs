0664b36c0072cf71b0bdb477d10b180bf9a1c8a0
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2024-04-01 16:55:29 +1300

    initial

---
+++ mediaBreakpoints.mjs
@@ -0,0 +1,7 @@
+export const small = '768'
+export const large = '1024'
+export const xtraLarge = '1600'
+
+export const smallText = 'small:'
+export const largeText = 'large:'
+export const xtraLargeText = 'xtraLarge:'
