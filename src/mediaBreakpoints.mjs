018b760be7a215ede02a1734ede7f093ecc721e8
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2024-04-03 14:19:00 +1300

    media breakpoints

--- src/mediaBreakpoints.mjs
+++ src/mediaBreakpoints.mjs
@@ -1,7 +1,12 @@
-export const small = '768'
+export const small = '640'
+export const medium = '768'
 export const large = '1024'
-export const xtraLarge = '1600'
+export const xtraLarge = '1280'
+export const crazyLarge = '1536'
 
-export const smallText = 'small?'
-export const largeText = 'large?'
-export const xtraLargeText = 'xtraLarge?'
+
+export const smallText = 'small'
+export const mediumText = 'small'
+export const largeText = 'large'
+export const xtraLargeText = 'xtraLarge'
+export const crazyLargeText = 'crazyLarge'
---
+++ src/processMedia.mjs
@@ -0,0 +1,46 @@
+
+import {
+  small,
+  large,
+  xtraLarge,
+  smallText,
+  largeText,
+  xtraLargeText,
+} from "../src/mediaBreakpoints.mjs";
+
+export function ProcessMedia(matches, colon) {
+    let output = "";
+    
+        for (const match of matches) {
+            console.log(match);
+      
+        let style = match.groups["style"];
+          let value = match.groups["value"];
+          let media = match.groups["media"];
+    
+          console.log(`MEDIA FOUND ${media} ${style} ${value}`);
+          let width;
+    
+          switch (media) {
+            case smallText:
+              width=small
+              break;
+            case largeText:
+              width=large
+              break;
+            case xtraLargeText:
+              width=xtraLarge
+              break;
+            default:
+              console.log("unknown media!!");
+          }
+    
+          output +=
+                `@media (min-width: ${width}px) {\r\n.${media}\\?${style}${colon == false ? '\\:' : '-'}${value} {\r\n\t` +
+                style +
+                ": " +
+                value +
+                `;\r\n}\r\n}\r\n`;
+            }
+            return output
+}
