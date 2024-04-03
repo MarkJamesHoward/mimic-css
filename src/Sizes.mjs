cc63e448c5915bd34645a38343c5c64d53882f1b
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2024-04-03 19:48:27 +1300

    fix margin-bottom

--- src/Sizes.mjs
+++ src/Sizes.mjs
@@ -1,30 +1,35 @@
-export const Sizes =
-{
-    xs: 'xs',
-    sm: 'sm',
-    md: 'md',
-    lg: 'lg',
-    xl: 'xl',
-    xl2:'2xl'
-}
+export const Sizes = {
+  xs: "xs",
+  sm: "sm",
+  md: "md",
+  lg: "lg",
+  xl: "xl",
+  xl2: "2xl",
+};
 
-export const FontSizes =
-{
-    xs: '8px',
-    sm: '12px',
-    md: '16px',
-    lg: '24px',
-    xl: '48px',
-    xl2: '92px'
-}
+export const FontSizes = {
+  xs: "8px",
+  sm: "12px",
+  md: "16px",
+  lg: "24px",
+  xl: "48px",
+  xl2: "92px",
+};
 
-export const PaddingSizes =
-{
-    xs: '2px',
-    sm: '4px',
-    md: '8px',
-    lg: '20px',
-    xl: '50px',
-    xl2: '200px'
-}
+export const PaddingSizes = {
+  xs: "2px",
+  sm: "4px",
+  md: "8px",
+  lg: "20px",
+  xl: "50px",
+  xl2: "200px",
+};
 
+export const GenericSizes = {
+  xs: "2px",
+  sm: "8px",
+  md: "20px",
+  lg: "40px",
+  xl: "60px",
+  xl2: "100px",
+};
--- src/performSnap.mjs
+++ src/performSnap.mjs
@@ -1,82 +1,109 @@
-import { Sizes, PaddingSizes, FontSizes } from './Sizes.mjs'
+import { Sizes, PaddingSizes, FontSizes, GenericSizes } from "./Sizes.mjs";
 
 export function PerformSnap(type, value) {
-    let result = 0
-    //  console.log(type)
-    //  console.log(value)
+  let result = 0;
+  //  console.log(type)
+  //  console.log(value)
 
-    switch (type) {
-        case 'padding-top':
-        case 'padding-left':
-        case 'padding-bottom':
-        case 'padding-right':
-            switch (value) {
-                case Sizes.xs :
-                    console.log('TINEY!!!  ' + PaddingSizes.xs)
-                    result = PaddingSizes.xs;
-                    break;
+  switch (type) {
+    case "padding-top":
+    case "padding-left":
+    case "padding-bottom":
+    case "padding-right":
+      switch (value) {
+        case Sizes.xs:
+          result = PaddingSizes.xs;
+          break;
 
-                case Sizes.sm :
-                    result = PaddingSizes.sm;
-                    break;
+        case Sizes.sm:
+          result = PaddingSizes.sm;
+          break;
 
-                case Sizes.md :
-                    result = PaddingSizes.md;
-                    break;
+        case Sizes.md:
+          result = PaddingSizes.md;
+          break;
 
-                case Sizes.lg :
-                    result = PaddingSizes.lg;
-                    break;
+        case Sizes.lg:
+          result = PaddingSizes.lg;
+          break;
 
-                case Sizes.xl :
-                    result = PaddingSizes.xl;
-                    break;
+        case Sizes.xl:
+          result = PaddingSizes.xl;
+          break;
 
-                case Sizes.xl2 :
-                    result = PaddingSizes.xl2;
-                    break;
+        case Sizes.xl2:
+          result = PaddingSizes.xl2;
+          break;
 
-                default :
-                    result = PaddingSizes.xs;
-                    break;
-            }
-            break;
-
-            case 'font-size':
-                switch (value) {
-                    case Sizes.xs :
-                        result = FontSizes.xs;
-                        break;
-    
-                    case Sizes.sm :
-                        result = FontSizes.sm;
-                        break;
-    
-                    case Sizes.md :
-                        result = FontSizes.md;
-                        break;
-    
-                    case Sizes.lg :
-                        result = FontSizes.lg;
-                        break;
-    
-                    case Sizes.xl :
-                        result = FontSizes.xl;
-                        break;
-    
-                    case Sizes.xl2 :
-                        result = FontSizes.xl2;
-                        break;
-    
-                    default :
-                        result = FontSizes.xs;
-                        break;
-                }
-                break;
         default:
-            result = value;
-            break
-    }
-    console.log('snapping return ' + result)
-    return result
+          result = PaddingSizes.xs;
+          break;
+      }
+      break;
+
+    case "font-size":
+      switch (value) {
+        case Sizes.xs:
+          result = FontSizes.xs;
+          break;
+
+        case Sizes.sm:
+          result = FontSizes.sm;
+          break;
+
+        case Sizes.md:
+          result = FontSizes.md;
+          break;
+
+        case Sizes.lg:
+          result = FontSizes.lg;
+          break;
+
+        case Sizes.xl:
+          result = FontSizes.xl;
+          break;
+
+        case Sizes.xl2:
+          result = FontSizes.xl2;
+          break;
+
+        default:
+          result = FontSizes.xs;
+          break;
+      }
+      break;
+    default:
+      switch (value) {
+        case Sizes.xs:
+          result = GenericSizes.xs;
+          break;
+
+        case Sizes.sm:
+          result = GenericSizes.sm;
+          break;
+
+        case Sizes.md:
+          result = GenericSizes.md;
+          break;
+
+        case Sizes.lg:
+          result = GenericSizes.lg;
+          break;
+
+        case Sizes.xl:
+          result = GenericSizes.xl;
+          break;
+
+        case Sizes.xl2:
+          result = GenericSizes.xl2;
+          break;
+
+        default:
+          result = GenericSizes.xs;
+          break;
+      }
+      break;
+  }
+  console.log("snapping return " + result);
+  return result;
 }
