c18c75b66f57d26cc44ac0e63ad4a643003961dc
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2024-05-02 19:12:57 +1200

    test files

--- mimic.css.js
+++ mimic.css.js
@@ -3,5 +3,11 @@ export const TWStyles = css`
 .text-decoration-color\\:\\#444444 {
 	text-decoration-color: #444444;
 }
+.text-decoration\\:underline {
+	text-decoration: underline;
+}
+.text-decoration-color\\:red {
+	text-decoration-color: red;
+}
  `
     
